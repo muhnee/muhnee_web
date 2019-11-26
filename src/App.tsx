d567bce590d60dd26f9e9778acdc0cf6060d9f29
Eric Jiang <ericjiang97@gmail.com>
2019-11-26 14:28:37 +1100

    [chore/cleanup]: remove unncessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import logo from "./logo.svg";
-import "./App.css";
 import LandingPage from "./pages/LandingPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
