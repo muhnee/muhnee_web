d60d53edded53451fbcc2b0ad946e0261e76fc0c
Eric Jiang <ericjiang97@gmail.com>
2019-11-30 20:47:57 +1100

    [feat/transactions]: ability to update existing transactions

--- src/pages/TransactionPage/TransactionPage.tsx
+++ src/pages/TransactionPage/TransactionPage.tsx
@@ -1,32 +1,52 @@
-import React, { FC, useContext, useState } from "react";
+import React, { FC, useContext, useState, useEffect } from "react";
+import moment from "moment";
 import firebase from "firebase";
 import { useParams, Redirect, useHistory } from "react-router-dom";
-import { useDocumentData } from "react-firebase-hooks/firestore";
+import { MaterialUiPickersDate } from "@material-ui/pickers/typings/date";
 
 import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import FormControl from "@material-ui/core/FormControl";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+import Switch from "@material-ui/core/Switch";
+import TextField from "@material-ui/core/TextField";
+
+import { DateTimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import MomentUtils from "@date-io/moment";
+
+import DeleteTransactionWarningDialog from "../../components/dialogs/DeleteTransactionWarningDialog";
 
 import LoadingContainer from "../../containers/LoadingContainer";
 
 import AuthenticationContext from "../../contexts/AuthenticationContext";
 
-import { Transaction } from "../../types/Transaction";
-
 import useStyles from "./styles";
-import DeleteTransactionWarningDialog from "../../components/dialogs/DeleteTransactionWarningDialog";
 
 const TransactionPage: FC = () => {
   const { user } = useContext(AuthenticationContext);
 
+  const [isLoading, setIsLoading] = useState(false);
   const [warningDialogOpen, setWarningDialogOpen] = useState(false);
+  const [type, setType] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [description, setDescription] = useState("");
+  const [taxDeductible, setTaxDeductible] = useState(false);
+  const [selectedDate, handleDateChange] = useState<MaterialUiPickersDate>(
+    moment()
+  );
 
   const history = useHistory();
   let { monthId, transactionId } = useParams();
   const classes = useStyles();
 
-  const [transaction, loading, error] = useDocumentData<Transaction>(
-    user
-      ? firebase
+  useEffect(() => {
+    async function getData() {
+      if (user && user.uid) {
+        setIsLoading(true);
+        const doc = await firebase
           .firestore()
           .collection("users")
           .doc(user.uid)
@@ -34,8 +54,30 @@ const TransactionPage: FC = () => {
           .doc(monthId)
           .collection("transactions")
           .doc(transactionId)
-      : null
-  );
+          .get();
+        if (doc.exists) {
+          const docData: any = doc.data();
+          setType(docData.type);
+          setAmount(docData.amount);
+          handleDateChange(moment(new Date(docData.timestamp.toDate())));
+          setDescription(docData.description);
+          setTaxDeductible(docData.taxDeductible);
+        }
+        setIsLoading(false);
+      }
+    }
+    getData();
+  }, [
+    monthId,
+    transactionId,
+    user,
+    setType,
+    setAmount,
+    handleDateChange,
+    setDescription,
+    setTaxDeductible,
+    setIsLoading
+  ]);
 
   const onDeleteTransaction = async () => {
     if (user) {
@@ -52,11 +94,44 @@ const TransactionPage: FC = () => {
     }
   };
 
+  const handleToggle = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    checked: boolean
+  ) => {
+    setTaxDeductible(checked);
+  };
+
+  const updateTransaction = () => {
+    if (user && selectedDate) {
+      firebase
+        .firestore()
+        .collection("users")
+        .doc(user.uid)
+        .collection("budget")
+        .doc(monthId)
+        .collection("transactions")
+        .doc(transactionId)
+        .update({
+          type,
+          amount: +amount,
+          description,
+          taxDeductible,
+          timestamp: selectedDate.toDate()
+        })
+        .then(() => {
+          history.push("/dashboard");
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
+  };
+
   if (!user || !user.uid) {
     return <Redirect to="/" />;
   }
 
-  if (loading) {
+  if (isLoading) {
     return (
       <div className={classes.root}>
         <LoadingContainer loadingMessage="Fetching Transaction Data..." />
@@ -64,49 +139,99 @@ const TransactionPage: FC = () => {
     );
   }
 
-  if (error) {
-    return (
-      <div className={classes.root}>
-        <Typography variant="h6" color="textPrimary">
-          An Error Occured.
-        </Typography>
-      </div>
-    );
-  }
-
-  if (!transaction) {
-    return (
-      <div className={classes.root}>
-        <Typography variant="h6" color="textPrimary">
-          Cannot Find Transaction
-        </Typography>
-        <Button variant="outlined" color="primary" href="/dashboard">
-          Go Back Home
-        </Button>
-      </div>
-    );
-  }
-
   return (
-    <div className={classes.root}>
-      <pre>{JSON.stringify(transaction, null, 2)}</pre>
-      <Button
-        variant="outlined"
-        color="primary"
-        onClick={() => {
-          setWarningDialogOpen(true);
-        }}
-      >
-        Delete Transaction
-      </Button>
-      <DeleteTransactionWarningDialog
-        open={warningDialogOpen}
-        onClose={() => {
-          setWarningDialogOpen(false);
-        }}
-        onDeleteTransaction={() => onDeleteTransaction()}
-      />
-    </div>
+    <>
+      <MuiPickersUtilsProvider utils={MomentUtils}>
+        <div className={classes.root}>
+          <>
+            <div className={classes.body}>
+              <FormControl>
+                <InputLabel id="transaction-type-label">Type</InputLabel>
+                <Select
+                  labelId="transaction-type-label"
+                  id="transaction-type"
+                  value={type}
+                  onChange={(event: React.ChangeEvent<{ value: unknown }>) => {
+                    setType(event.target.value as string);
+                  }}
+                >
+                  <MenuItem value={"expense"}>Expense</MenuItem>
+                  <MenuItem value={"income"}>Income</MenuItem>
+                </Select>
+              </FormControl>
+              <TextField
+                required
+                id="amount"
+                label="Amount"
+                value={amount}
+                margin="normal"
+                type="number"
+                onChange={(event: React.ChangeEvent<{ value: unknown }>) => {
+                  setAmount(event.target.value as number);
+                }}
+              />
+              <TextField
+                required
+                id="description"
+                label="Description"
+                value={description}
+                margin="normal"
+                onChange={(event: React.ChangeEvent<{ value: unknown }>) => {
+                  setDescription(event.target.value as string);
+                }}
+              />
+              <FormControlLabel
+                control={
+                  <Switch
+                    checked={taxDeductible}
+                    onChange={handleToggle}
+                    value="taxDeductible"
+                    inputProps={{ "aria-label": "secondary checkbox" }}
+                    color="primary"
+                  />
+                }
+                label="Tax Deductible?"
+                className={classes.switch}
+              />
+              <DateTimePicker
+                label="Transaction Date Time"
+                inputVariant="outlined"
+                value={selectedDate}
+                onChange={handleDateChange}
+              />
+            </div>
+            <Divider />
+            <div>
+              <Button
+                variant="outlined"
+                color="primary"
+                onClick={() => {
+                  updateTransaction();
+                }}
+              >
+                Update Transaction
+              </Button>
+              <Button
+                variant="outlined"
+                color="primary"
+                onClick={() => {
+                  setWarningDialogOpen(true);
+                }}
+              >
+                Delete Transaction
+              </Button>
+            </div>
+          </>
+        </div>
+        <DeleteTransactionWarningDialog
+          open={warningDialogOpen}
+          onClose={() => {
+            setWarningDialogOpen(false);
+          }}
+          onDeleteTransaction={() => onDeleteTransaction()}
+        />
+      </MuiPickersUtilsProvider>
+    </>
   );
 };
 
