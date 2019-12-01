a60eccd1b00b22da1800841f14bc542d3c648836
Eric Jiang <ericjiang97@gmail.com>
2019-12-01 21:08:47 +1100

    [fix/ui]: display month even when this no transaction

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -67,23 +67,27 @@ const DashboardPage: FC = () => {
         <Typography variant="h6" color="primary">
           {`This Month (${today.format("MMMM YYYY")})`}
         </Typography>
-        {summary && (
-          <div className={classes.summaryContainer}>
-            <SummaryCard
-              title="Expenses"
-              amount={
-                summary.expenses ? `$${summary.expenses.toFixed(2)}` : "N/A"
-              }
-              avatar={<ExpenseIcon />}
-              inverted
-            />
-            <SummaryCard
-              title="Income"
-              avatar={<IncomeIcon />}
-              amount={summary.income ? `$${summary.income.toFixed(2)}` : "N/A"}
-            />
-          </div>
-        )}
+        <div className={classes.summaryContainer}>
+          <SummaryCard
+            title="Expenses"
+            amount={
+              summary && summary.expenses
+                ? `$${summary.expenses.toFixed(2)}`
+                : "N/A"
+            }
+            avatar={<ExpenseIcon />}
+            inverted
+          />
+          <SummaryCard
+            title="Income"
+            avatar={<IncomeIcon />}
+            amount={
+              summary && summary.income
+                ? `$${summary.income.toFixed(2)}`
+                : "N/A"
+            }
+          />
+        </div>
       </div>
       <div className={classes.rightContainer}>
         <Typography variant="body1">Recent Transactions</Typography>
