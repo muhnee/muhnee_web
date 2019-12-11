996e487f12e2b8461ffc2b1fad0c8a02a1a3eb15
Eric Jiang <ericjiang97@gmail.com>
2019-12-11 21:25:07 +1100

    [feat/ui]: loading animation for avatar icons

--- src/components/CategoryIconAvatar/CategoryIconAvatar.tsx
+++ src/components/CategoryIconAvatar/CategoryIconAvatar.tsx
@@ -2,6 +2,7 @@ import React, { FC, useEffect, useState } from "react";
 import firebase from "firebase";
 
 import Avatar from "@material-ui/core/Avatar";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 import { CategoryIconAvatarProps } from "./types";
 import { useNotificationDispatch } from "../../contexts/NotificationProvider";
@@ -10,26 +11,45 @@ const CategoryIconAvatar: FC<CategoryIconAvatarProps> = ({
   onClick = () => {},
   category
 }) => {
+  const [isLoading, setIsLoading] = useState(false);
   const [avatarIcon, setAvatarIcon] = useState("");
   const dispatchNotifications = useNotificationDispatch();
 
   useEffect(() => {
     async function getIcon() {
+      setIsLoading(true);
       if (category && category.icon) {
         const firebaseRef = firebase
           .storage()
           .refFromURL(`gs://muhnee-app.appspot.com/${category.icon}`);
+
         try {
           setAvatarIcon(await firebaseRef.getDownloadURL());
+          setIsLoading(false);
         } catch (e) {
           setAvatarIcon("");
+          setIsLoading(false);
         }
       } else {
         setAvatarIcon("");
+        setIsLoading(false);
       }
     }
     getIcon();
-  }, [avatarIcon, category, dispatchNotifications, setAvatarIcon]);
+  }, [
+    avatarIcon,
+    category,
+    dispatchNotifications,
+    setAvatarIcon,
+    setIsLoading
+  ]);
+  if (isLoading) {
+    return (
+      <Avatar>
+        <CircularProgress />
+      </Avatar>
+    );
+  }
 
   return <Avatar onClick={onClick} src={avatarIcon} />;
 };
