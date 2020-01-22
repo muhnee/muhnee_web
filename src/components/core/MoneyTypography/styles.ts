96eccd6d4ed5b2e4d17b38891c60014e1ee3e71d
Eric Jiang <ericjiang97@gmail.com>
2020-01-22 14:38:21 +1100

    [feat/ui] - MUW-73 : made SummaryCard more pretty

--- src/components/core/MoneyTypography/styles.ts
+++ src/components/core/MoneyTypography/styles.ts
@@ -1,10 +1,14 @@
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import { red } from "@material-ui/core/colors";
 
-const useStyles = makeStyles<Theme>(theme =>
+import MoneyTypographyProps from "./types";
+
+const useStyles = makeStyles<Theme, MoneyTypographyProps>(theme =>
   createStyles({
-    root: {
-      fontFamily: "Varela Round, Montserrat"
-    }
+    root: ({ type }) => ({
+      fontFamily: "Varela Round, Montserrat",
+      color: type === "expense" ? red[500] : "#777"
+    })
   })
 );
 
