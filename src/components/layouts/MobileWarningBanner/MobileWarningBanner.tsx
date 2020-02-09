4fb0e02606df12d299fbb82c7b07f98c9dbc8126
Eric Jiang <ericjiang97@gmail.com>
2020-02-09 12:27:34 +1100

    logic for mobileWarningBanner

--- src/components/layouts/MobileWarningBanner/MobileWarningBanner.tsx
+++ src/components/layouts/MobileWarningBanner/MobileWarningBanner.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import useStyles from "./styles";
 import { IconButton } from "@material-ui/core";
 
@@ -7,13 +7,26 @@ import CloseIcon from "@material-ui/icons/Close";
 const MobileWarningBanner: FC = () => {
   const [displayed, setDisplayed] = useState(true);
   const classes = useStyles();
+
+  useEffect(() => {
+    const localStorage = window.localStorage;
+    const isDismissed = localStorage.getItem("mobileDismissed") || false;
+
+    setDisplayed(!isDismissed);
+  }, [setDisplayed]);
+
   return (
     <div
       className={classes.root}
       style={{ display: displayed ? "flex" : "none" }}
     >
       <div style={{ flex: 1 }}>Try the Muhnee Mobile App Today!</div>
-      <IconButton onClick={() => setDisplayed(false)}>
+      <IconButton
+        onClick={() => {
+          setDisplayed(false);
+          window.localStorage.setItem("mobileDismissed", "true");
+        }}
+      >
         <CloseIcon />
       </IconButton>
     </div>
